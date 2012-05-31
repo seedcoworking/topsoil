781efe59f80ee50508724ab715e564f475868119
Jamie Wright <jamie@brilliantfantastic.com>
2012-05-31 18:55:09 -0400

    Initial rails-api application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Topsoil::Application.config.secret_token = 'b0f88c84f3fc1209b3a6f5d7b8a388c473e1d2886afe5027fe866c820cbfb35c4e70f673ddc5c60baf8b055242d02a5dda653f06896b61b8c4a2cd991edc45dc'
