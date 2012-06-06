cc66219de9385281ac998db60018047ca0d5f22c
Jamie Wright <jamie@brilliantfantastic.com>
2012-06-05 23:25:27 -0400

    Added several validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   validates_uniqueness_of :email, :case_sensitive => false
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  before :all do
+  before :each do
     @subject = FactoryGirl.create :user
   end
 
   it 'validates the uniqueness of email' do
     product = FactoryGirl.build :user
     product.should_not be_valid
+    product.errors[:email].should include 'has already been taken'
+  end
+
+  it 'validates the case insensitivity of email' do
+    product = FactoryGirl.build :user
+    product.email = @subject.email.upcase
+    product.should_not be_valid
+    product.errors[:email].should include 'has already been taken'
+  end
+
+  it 'validates a valid email address' do
+    product = FactoryGirl.build :user, :email => 'invalid@example'
+    product.should_not be_valid
+    product.errors[:email].should include 'is invalid'
   end
 end
